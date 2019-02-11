36460f0c621e379c8511c72d62c4a1006cb6c0f3
jwhaeke <jwhaeke@live.nl>
2019-02-11 15:45:21 +0100

    Added base investment model, migration and controller

---
+++ app/Http/Controllers/InvestmentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\investment;
+use Illuminate\Http\Request;
+
+class InvestmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\investment  $investment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(investment $investment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\investment  $investment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(investment $investment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\investment  $investment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, investment $investment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\investment  $investment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(investment $investment)
+    {
+        //
+    }
+}
