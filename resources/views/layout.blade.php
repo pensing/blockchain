286d5f94cf6aece8794902aad04bb874588e9c4e
jwhaeke <jwhaeke@live.nl>
2019-02-11 15:35:51 +0100

    Added some basic features, such as a layout and error file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title> @yield('title', 'Blockchain') </title>
+   
+</head>
+<body>
+   <div class="container">
+       @yield('content')
+   </div>
+</body>
+</html>
