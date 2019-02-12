e91975b1845c9594324a19879987a8a7c143ce80
vagrant <vagrant@homestead>
2019-02-12 16:20:00 +0000

    Merge error

--- app/UserRol.php
+++ app/UserRol.php
@@ -4,10 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserRol extends Model
+class Userrol extends Model
 {
     protected $quarded = [];
     public function userRol(){
         return $this->hasOne('App\User');
-    } 
+    }
 }
